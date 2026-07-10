401491f66ada99a20b38d5f16c545dc0837e8344
Akshay S B <45756984+itsakshaysb@users.noreply.github.com>
2026-07-10 18:29:18 +0000

    day 2 execises done

--- day2.py
+++ day2.py
@@ -1,1 +1,35 @@
-print("Hello, World!")
+print ('Day 2: 30 Days of python programming')
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+full_name = first_name + ' ' + last_name
+country = 'Finland'
+city = 'Helsinki'
+age = 250
+year = 2020
+is_married = True
+is_light_on = False
+x,y,z = 1,2,3
+type(first_name)
+type(age)
+type(is_light_on)
+print(len(first_name))
+print(len(last_name))
+length = len(first_name) + len(last_name)
+print(length)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+area_of_circle = 3.14 * 30 ** 2
+radius = 30
+circum_of_circle = 2 * 3.14 * radius
+rad=int(input('Enter radius: '))
+inputname = input('Enter your name: ')
+inputage = int(input('Enter your age: '))
+inputcountry = input('Enter your country: ')
+inputage = int(input('Enter your age: '))
