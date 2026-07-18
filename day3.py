12f5b77be2d5939e1b897c12a2ce4075cd526dac
Akshay S B <45756984+itsakshaysb@users.noreply.github.com>
2026-07-18 16:56:34 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,16 @@
+"""age = 20
+height2 = 180.0
+complex_n= complex(1,3)
+height = int(input("what is the height ?"))
+base =int(input("what is the base length?"))
+print (f"area of the the triangle is {.5*height*base**2}")
+l=int(input("what is length"))
+w=int(input("what is the width"))
+print (f"area of the rectangle is {l*w}and perieter is {2*(l+w)}")4
+
+rad= int(input("what is the radious of the circle "))
+print(f"area of the circle{3.14*rad**2} and the circumferance is {2*3.14*rad}")"""
+
+p=len("python")
+d= len("dragon")
+print(p>d)
