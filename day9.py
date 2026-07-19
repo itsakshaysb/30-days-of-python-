e85fc63aaf4fea1a0c8feaa2c006cded1bbb4d98
Akshay S B <45756984+itsakshaysb@users.noreply.github.com>
2026-07-19 16:01:37 +0000

    Day 9

---
+++ day9.py
@@ -0,0 +1,46 @@
+# age= int(input("what is your age ?"))
+# # if (age) >= 18:
+# #     print("old enough to drive ")
+# # else :
+# #     print(f"you need {18-age} years to drive")
+# my_age = 20
+# if age>my_age:
+#     print (f" you are {age-my_age} years older than me")
+# else :
+#     print (f" you are {my_age-age} years younger than me")
+
+# score= int(input("what us your score ?"))
+# if score in range(90,101):
+#     print(" your grade is A")
+# elif score in range(80,90):
+#     print(" your grade is b")
+# elif score in range(70,80):
+#     print(" your grade is c")
+# elif score in range(60,70):
+#     print(" your grade is d")
+# else: 
+#     print("your grade is F ")
+    
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# new= input(" new fruit ")
+# if new in fruits:
+#     print (fruits)
+# else :
+#     fruits.append(new)
+#     print(fruits)
+
+
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+
+    
