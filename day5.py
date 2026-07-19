d934e0c8f6638528f1ce1887477bbcbe3af2aa97
Akshay S B <45756984+itsakshaysb@users.noreply.github.com>
2026-07-19 07:20:12 +0000

    day 5

---
+++ day5.py
@@ -0,0 +1,12 @@
+lit =[]
+lit2 = ["car","dog","cow","hourse","donkey"]
+print(len(lit2))
+print(lit2[1])
+print(lit2[3])
+print(lit2[-1])
+mixed_list=["name", 2,124,True,"world"]
+it_companies=["facebook","google", "microsoft","apple", "ibm"]
+print(it_companies)
+
+
+bla blaa 
