0f771be75b892d159a035a025ac846c1feb65a2f
Akshay S B <45756984+itsakshaysb@users.noreply.github.com>
2026-07-19 07:58:54 +0000

    day 6

---
+++ day6.py
@@ -0,0 +1,13 @@
+tup=()
+sis=("apple", "orangre")
+bro=("carrot","onion")
+com=sis+bro
+print(com)
+print(len(com))
+famaly_mem=("jam","curry")+com
+print(famaly_mem)
+parent1, parent2, *siblings =famaly_mem
+print(parent1)  #
+print(parent2)  #
+print(siblings) # 
+
