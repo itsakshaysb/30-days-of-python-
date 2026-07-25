e577e872ce10c57de730a07ccef88a165dd345dd
Akshay <AKSHAYSBABU@ICLOUD.COM>
2026-07-26 00:59:27 +0530

    Add two files to the repository

--- day17.py
+++ day17.py
@@ -1,1 +1,12 @@
-print("hi")
+try:
+    print(10 + 5)
+except:
+    print('Something went wrong')
+
+try:
+    name = input('Enter your name:')
+    year_born = int(input('Year you were born:'))
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except:
+    print('Something went wrong')
